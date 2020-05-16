8782f37ae8f72b9f41b45a70f4d8ecb7a41764e0
aineon <enia.oneill@gmail.com>
2020-05-16 19:20:54 +0000

    JSON.parse Lesson

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-let xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest(); //creates a new instance of the object
+let data; //to store the data from responseText so it can be manipulated
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); //retrieves the data from the URL entered
+xhr.send(); // sends the request to retrieve the data
+
 
 xhr.onreadystatechange =function() {
-    if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("data").innerHTML = this.responseText; 
+    if (this.readyState == 4 && this.status == 200) {  //readystate==4 means the operation has been completed. status ==200 (http status code) means request succeeded, content delivered
+       //document.getElementById("data").innerHTML = this.responseText; code to get the response into our div
+       //console.log(typeof(this.responseText)); checks the type of variable - in this case a string
+       //console.log(typeof(JSON.parse(this.responseText))); //parses the string into a JSON data structure (object)
+       //console.log(JSON.parse(this.responseText)); //logs the actual object to the console
+       data = this.responseText;
     }
 };
-
+console.log(data); //however this returns undefined
