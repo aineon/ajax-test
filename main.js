2fbb18bdeb65be7b38bb1ef77efd556743f01888
aineon <enia.oneill@gmail.com>
2020-05-17 13:39:36 +0000

    getting functional lesson

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let data; //to store the data from responseText so it can be manipulated
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); //retrieves the data from the URL entered
 xhr.send(); // sends the request to retrieve the data
 
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+}
 
 xhr.onreadystatechange =function() {
     if (this.readyState == 4 && this.status == 200) {  //readystate==4 means the operation has been completed. status ==200 (http status code) means request succeeded, content delivered
@@ -11,7 +15,7 @@ xhr.onreadystatechange =function() {
        //console.log(typeof(this.responseText)); checks the type of variable - in this case a string
        //console.log(typeof(JSON.parse(this.responseText))); //parses the string into a JSON data structure (object)
        //console.log(JSON.parse(this.responseText)); //logs the actual object to the console
-       data = this.responseText;
+       setData(JSON.parse(this.responseText));
     }
 };
-console.log(data); //however this returns undefined
+
